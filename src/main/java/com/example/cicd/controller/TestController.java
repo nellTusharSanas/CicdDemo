d281777bf049d77a239e37216d560d35654b86cc
nellTusharSanas <39511768+nellTusharSanas@users.noreply.github.com>
2021-02-16 04:37:21 -0800

    first commit

---
+++ src/main/java/com/example/cicd/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.example.cicd.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	
+	@GetMapping("/getUserdata")
+	List<String> getUserdata()
+	{
+		List<String>lst=new ArrayList<String>();
+		lst.add("tushar");
+		lst.add("sanas");
+		return lst;
+	}
+
+}
